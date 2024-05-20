3d155ad6cec4bf423e2f55497605a4a13e409117
Adyatma <69962558+lafuanh@users.noreply.github.com>
2024-05-20 16:05:06 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project_Structdat
+//
+//  Created by naufal adyatma on 20/05/24.
+//
+
+#include <iostream>
+void displayMainMenu();
+
+int main(int argc, const char * argv[]) {
+    displayMainMenu();
+    std::cout << "Hello, World!\n";
+    return 0;
+}
