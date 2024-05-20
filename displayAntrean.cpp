1ff9e985382067b137cd80f80c5a92fb8d132824
Adyatma <69962558+lafuanh@users.noreply.github.com>
2024-05-20 16:05:58 +0700

    Create displayAntrean.cpp

---
+++ displayAntrean.cpp
@@ -0,0 +1,27 @@
+//
+//  displayAntrean.cpp
+//  UTS_StructurData
+//
+//  Created by naufal adyatma on 20/05/24.
+//
+
+#include <iostream>
+
+using namespace std;
+
+void displayAntrean(){
+    bool isRun = true;
+    string nama;
+    while (isRun) {
+        
+        cout << "input nama: ";
+        cin.ignore();
+        getline(cin, nama);
+        //search name
+        // if nama doesnt appear in patient -> show menu add patient to patient list call func
+        
+        // add form anteran
+        //add from antrean to queue
+        
+    }
+}
